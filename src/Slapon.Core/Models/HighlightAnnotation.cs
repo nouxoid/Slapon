fd98b97e07584c4b58dfe444086a9d2416c113ca
noux <arno.koopman@gmail.com>
2024-11-14 16:06:44 +0200

    adding highlighter tool

---
+++ src/Slapon.Core/Models/HighlightAnnotation.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using Slapon.Core.Interfaces;
+
+namespace Slapon.Core.Models;
+
+public class HighlightAnnotation : IAnnotation
+{
+    public RectangleF Bounds { get; set; }
+    public Color Color { get; set; }
+    public bool IsSelected { get; set; }
+    private const float HIGHLIGHT_OPACITY = 0.4f;
+
+    public HighlightAnnotation(RectangleF bounds, Color color)
+    {
+        Bounds = bounds;
+        Color = color;
+    }
+
+    public void Draw(Graphics g)
+    {
+        using (var highlightBrush = new SolidBrush(Color.FromArgb((int)(255 * HIGHLIGHT_OPACITY), Color)))
+        {
+            g.FillRectangle(highlightBrush, Bounds);
+        }
+
+        if (IsSelected)
+        {
+            using (var pen = new Pen(Color.Black, 1) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dash })
+            {
+                g.DrawRectangle(pen, Rectangle.Round(Bounds));
+            }
+        }
+    }
+
+    public bool Contains(PointF point)
+    {
+        return Bounds.Contains(point);
+    }
+
+    public void MoveTo(PointF location)
+    {
+        Bounds = new RectangleF(location, Bounds.Size);
+    }
+}
