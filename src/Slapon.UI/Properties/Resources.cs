65dc7a5f937c4032d4332001aecb0cef15a860ea
noux <arno.koopman@gmail.com>
2024-11-16 10:14:55 +0200

    added icons and working futher on the toolbar layout.

---
+++ src/Slapon.UI/Properties/Resources.cs
@@ -0,0 +1,25 @@
+﻿namespace Slapon.UI.Properties
+{
+    internal class Resources
+    {
+        private static System.Drawing.Image? LoadImage(string path)
+        {
+            try
+            {
+                return System.Drawing.Image.FromFile(path);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static readonly string IconPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "icons");
+
+        public static System.Drawing.Image? rectangle => LoadImage(Path.Combine(IconPath, "rectangle.png"));
+        public static System.Drawing.Image? highlighter => LoadImage(Path.Combine(IconPath, "highlighter.png"));
+        public static System.Drawing.Image? line => LoadImage(Path.Combine(IconPath, "line.png"));
+        public static System.Drawing.Image? text => LoadImage(Path.Combine(IconPath, "text.png"));
+        public static System.Drawing.Image? clearall => LoadImage(Path.Combine(IconPath, "clearall.png"));
+    }
+}
